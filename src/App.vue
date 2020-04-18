498a40d0c30290bd16c7e89dc2f4a5c9995bf2c4
Tair Abduraimov <abduraimovtair@gmail.com>
2020-04-18 20:22:56 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NoteManager title="Note Manager"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NoteManager from './components/NoteManager.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NoteManager
   }
 }
 </script>
@@ -21,8 +20,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  margin-left: 10vw;
+  margin-right: 10vw;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
