1e281612e70126af2b061da7981d1de317b51f1d
Tair Abduraimov <abduraimovtair@gmail.com>
2020-04-19 17:02:55 +0300

    create NoteEditor

---
+++ src/components/NoteEditor.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="note-editor shadow">
+    <button
+      type="button"
+      @click="saveNote"
+      class="note-button btn btn-sm btn-outline-secondary"
+    >Save</button>
+    <button
+      type="button"
+      @click="lastChange"
+      class="note-button btn btn-sm btn-outline-secondary"
+    >&#8592;</button>
+    <button
+      type="button"
+      @click="returnChange"
+      class="note-button btn btn-sm btn-outline-secondary"
+    >&#8594;</button>
+    <button
+      type="button"
+      @click="deleteNote"
+      class="note-button btn btn-sm btn-outline-secondary"
+    >Delete</button>
+    <button
+      type="button"
+      @click="closeNote"
+      class="note-button btn btn-sm btn-outline-secondary"
+    >Close</button>
+    <div class="editor-body">
+      <h5 class="editor-title">{{note.title}}</h5>
+      <button type="button" @click="addTodo" class="note-button btn btn-sm btn-outline-dark">Add</button>
+      <div class="todo-list">
+        <div class="todo-item" v-for="(item, idx) in note.list" :key="idx">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <input type="checkbox" v-model="item.status" />
+              </div>
+            </div>
+            <textarea class="form-control" v-model="item.name" />
+            <button @click="deleteTodo" class="btn btn-outline-dark" type="button">
+              <svg
+                class="bi bi-trash"
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoteEditor",
+  props: {
+    note: null
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    closeNote() {
+      let me = this;
+      me.$emit("close");
+    },
+    saveNote() {},
+    deleteNote() {},
+    lastChange() {},
+    returnChange() {},
+    addTodo() {},
+    deleteTodo() {}
+  }
+};
+</script>
+
+<style scoped>
+.note-editor {
+  margin: auto;
+  width: calc(100vw - 100px);
+  max-width: 500px;
+  padding: 20px;
+}
+.note-button {
+  margin: 5px;
+}
+.todo-list {
+  margin: 2px;
+  padding: 15px 10px;
+  border: 1px solid gray;
+  border-radius: 8px;
+}
+.editor-title {
+  margin: 15px 10px;
+  text-align: center;
+}
+.todo-input {
+  margin-right: 10px;
+}
+</style>
--- src/components/NoteManager.vue
+++ src/components/NoteManager.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="hello">
-    <h1 class="note-manager-title">{{ title }}</h1>
-    <div class="buttons">
-      <button type="button" class="note-manager-crud-button btn btn-sm btn-outline-secondary">Add</button>
-      <button type="button" class="note-manager-crud-button btn btn-sm btn-outline-secondary">Edit</button>
-      <button type="button" class="note-manager-crud-button btn btn-sm btn-outline-secondary">Delete</button>
-    </div>
-    <div class="notes-container">
-      <div class="note-card shadow" @click="onNoteClick(note)" @dblclick="onNoteDblClick(note)" v-for="(note, index) in notes" :key="index" :id="'note' + note.id">
-        <div class="card-body">
-          <h5 class="card-title">{{note.title}}</h5>
-          <div class="todo-list">
-            <div class="todo-item" v-for="(item, idx) in note.list" :key="idx">
-              <div v-if="idx < numOfItems">
-                <input class="form-check-input" type="checkbox" disabled :checked="item.status">
-                <label class="form-check-label">
-                  {{item.name}}
-                </label>
+  <div>
+    <div v-if="!editMode">
+      <h1 class="note-manager-title">{{ title }}</h1>
+      <div class="buttons">
+        <button type="button" @click="addItem" class="crud-button btn btn-sm btn-outline-secondary">Add</button>
+        <button type="button" @click="editItem" class="crud-button btn btn-sm btn-outline-secondary">Edit</button>
+        <button type="button" @click="deleteItem" class="crud-button btn btn-sm btn-outline-secondary">Delete</button>
+      </div>
+      <div class="notes-container">
+        <div class="note-card shadow" @click="onNoteClick(note)" @dblclick="onNoteDblClick(note)" v-for="(note, index) in notes" :key="index" :id="'note' + note.id">
+          <div class="card-body">
+            <h5 class="card-title">{{note.title}}</h5>
+            <div class="todo-list">
+              <div class="todo-item" v-for="(item, idx) in note.list" :key="idx"> <div v-if="idx < numOfItems">   <input class="todo-input" type="checkbox" disabled :checked="item.status">   <label class="todo-label">     {{item.name}}   </label> </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <note-editor v-if="editMode" :note="selected[0]" @close="closeEditCard"></note-editor>
   </div>
 </template>
 
 <script>
+import NoteEditor from './NoteEditor.vue'
 export default {
   name: 'NoteManager',
   props: {
@@ -36,15 +34,26 @@ export default {
     let me = this;
     return {
       notes: me.initNotes(),
-      numOfItems: 4
+      selected: [],
+      numOfItems: 4,
+      editMode: false,
+      clickedObj: {}
     }
+  },
+  components: {
+    "note-editor": NoteEditor
+  },
+  created() {
+    let me = this;
+    me.clickedObj = me.createClickedObj();
   },
   methods: {
     initNotes() {
+      let me = this;
       return [
         {
           title: "Project X",
-          id: 1,
+          id: me.genID(),
           list: [
             {
               name: "drink a lot",
@@ -62,7 +71,7 @@ export default {
         },
         {
           title: "Work tasks",
-          id: 2,
+          id: me.genID(),
           list: [
             {
               name: "code web page",
@@ -76,7 +85,7 @@ export default {
         },
         {
           title: "Traveling",
-          id: 3,
+          id: me.genID(),
           list: [
             {
               name: "Prague",
@@ -110,53 +119,40 @@ export default {
         },
         {
           title: "J Cole 'Middle Child'",
-          id: 4,
+          id: me.genID(),
           list: [
             {
-              name: `Niggas been countin' me out
-                I’m countin' my bullets, I'm loadin’ my clips
-                I'm writin' down names, I'm makin' a list`,
+              name: `Niggas been countin' me out I’m countin' my bullets, I'm loadin’ my clips I'm writin' down names, I'm makin' a list`,
               status: false
             },
             {
-              name: `I'm all in my bag, this hard as it get
-                I do not snort powder, I might take a sip
-                I might hit the blunt, but I'm liable to trip`,
+              name: `I'm all in my bag, this hard as it get I do not snort powder, I might take a sip I might hit the blunt, but I'm liable to trip`,
               status: false
             },
             {
-              name: `I'm all in my bag, this hard as it get
-                I do not snort powder, I might take a sip
-                I might hit the blunt, but I'm liable to trip`,
+              name: `I'm all in my bag, this hard as it get I do not snort powder, I might take a sip I might hit the blunt, but I'm liable to trip`,
               status: false
             },
             {
-              name: `I'm all in my bag, this hard as it get
-                I do not snort powder, I might take a sip
-                I might hit the blunt, but I'm liable to trip`,
+              name: `I'm all in my bag, this hard as it get I do not snort powder, I might take a sip I might hit the blunt, but I'm liable to trip`,
               status: false
             },
             {
-              name: `I'm all in my bag, this hard as it get
-                I do not snort powder, I might take a sip
-                I might hit the blunt, but I'm liable to trip`,
+              name: `I'm all in my bag, this hard as it get I do not snort powder, I might take a sip I might hit the blunt, but I'm liable to trip`,
               status: false
             },
             {
-              name: `I'm all in my bag, this hard as it get
-                I do not snort powder, I might take a sip
-                I might hit the blunt, but I'm liable to trip`,
+              name: `I'm all in my bag, this hard as it get I do not snort powder, I might take a sip I might hit the blunt, but I'm liable to trip`,
               status: false
             }
           ]
         },
         {
           title: "Lil Baby 'Yes Indeed'",
-          id: 5,
+          id: me.genID(),
           list: [
             {
-              name: `The dash, it's digi', the schedule busy
-                My head in a hoodie, my shorty a goodie`,
+              name: `The dash, it's digi', the schedule busy My head in a hoodie, my shorty a goodie`,
               status: false
             },
             {
@@ -179,11 +175,10 @@ export default {
         }, 
         {
           title: "King’s Dead",
-          id: 6,
+          id: me.genID(),
           list: [
             {
-              name: `My bitch been ready, my clique been ready
-                My shit's been ready, my check's been ready`,
+              name: `My bitch been ready, my clique been ready My shit's been ready, my check's been ready`,
               status: false
             },
             {
@@ -191,18 +186,15 @@ export default {
               status: false
             },
             {
-              name: `And it's like that, lil' bitch
-                MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
+              name: `And it's like that, lil' bitch MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
               status: false
             },
             {
-              name: `And it's like that, lil' bitch
-                MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
+              name: `And it's like that, lil' bitch MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
               status: false
             },
             {
-              name: `And it's like that, lil' bitch
-                MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
+              name: `And it's like that, lil' bitch MVP, I don't get no sleep, no, I don't like that, lil' bitch`,
               status: false
             },
           ]
@@ -210,18 +202,63 @@ export default {
       ]
     },
     onNoteClick(note) {
-      if (note.clicked) {
+      let me = this;
+      if (me.clickedObj[note.id]) {
         document.querySelector('#note' + note.id).style["background-color"] = null;
+        me.removeById(me.selected, note.id);
       } else {
         document.querySelector('#note' + note.id).style["background-color"] = "#b9bec4";
+        me.selected.push(note);
       }
-      note.clicked = !note.clicked;
+      me.clickedObj[note.id] = !me.clickedObj[note.id];
     },
     onNoteDblClick(note) {
-      console.log(note);
+      let me = this;
+      setTimeout(() => {
+        me.selected = [note];
+        me.editItem();
+      }, 20);   
+    },
+    addItem() {
+
+    },
+    editItem() {
+      let me = this;
+      if (me.selected.length > 0) {
+        if (me.clickedObj[me.selected[0].id]) {
+          me.clickedObj[me.selected[0].id] = false;
+        }
+        me.editMode = true;
+      }
+    },
+    closeEditCard() {
+      let me = this;
+      me.selected = [];
+      me.editMode = false;
+    },
+    deleteItem() {
+
+    },
+    createClickedObj() {
+      let me = this;
+      let res = {};
+      me.notes.forEach(element => {
+        res[element.id] = false;
+      });
+      return res;
+    },
+    removeById(array, id) {
+      let ind = array.findIndex(el => {
+        return el.id == id;
+      });
+      if (ind > -1) {
+        array.splice(ind, 1);
+      }
+    },
+    genID () {
+      return '_' + Math.random().toString(36).substr(2, 9);
     }
   }
-
 }
 </script>
 
@@ -230,15 +267,15 @@ export default {
   text-align: center;
 }
 .buttons {
-  margin:  10px;
+  margin:  10px 30px;
 }
-.note-manager-crud-button {
+.crud-button {
   margin: 5px;
 }
 .notes-container {
   margin: 20px 10px;
   display: flex;
-  flex-wrap: wrap
+  flex-wrap: wrap;
 }
 .note-card {
   min-width: 250px;
@@ -263,9 +300,12 @@ export default {
 }
 .todo-item {
   height: 1.5em;
-  margin: 10px 20px 10px 30px;
+  margin: 10px 20px 10px 10px;
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+}
+.todo-input {
+  margin-right: 10px;
 }
 </style>
